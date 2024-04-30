caa02a058955e61d7b19c66d4ba5babaed019ecd
JazaeloG <epicgamesbbass@gmail.com>
2024-04-29 18:00:26 -0600

    crud persona

---
+++ src/main/java/org/uv/BDNCPractica02/ControllerPersona.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/SpringFramework/RestController.java to edit this template
+ */
+package org.uv.BDNCPractica02;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/url")
+public class ControllerPersona {
+    @Autowired
+    private RepositoryPersona repositoryPersona;
+    
+    @GetMapping()
+    public List<Persona> list() {
+        return repositoryPersona.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Persona get(@PathVariable String id) {
+        Optional<Persona> optPersona = repositoryPersona.findById(id);
+        if(optPersona.isPresent())
+            return optPersona.get();
+        else
+            return null;
+    }
+    
+    @PutMapping("/{id}")
+    public Persona put(@PathVariable String id, @RequestBody Persona personaDetails) {
+        Optional<Persona> optPersona = repositoryPersona.findById(id);
+        if(optPersona.isPresent()){
+            Persona persona = optPersona.get();
+            persona.setNombre(personaDetails.getNombre());
+            persona.setDireccion(personaDetails.getDireccion());
+            persona.setTelefono(personaDetails.getTelefono());
+            return repositoryPersona.save(persona);
+        } else {
+            return null; 
+        }
+    }
+    
+    @PostMapping
+    public Persona post(@RequestBody Persona personaDetails) {
+        return repositoryPersona.save(personaDetails);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        repositoryPersona.deleteById(id);
+    }
+    
+}
